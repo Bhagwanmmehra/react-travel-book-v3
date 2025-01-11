75b4c42e8446175cea399e19b8aad956e7e1e315
bhagwanmmehra <bhagwanmehra04@gmail.com>
2025-01-11 23:58:08 +0530

    folder structure done

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,19 @@
+import Nav from "../Components/Nav";
+
+function HomePage() {
+  return (
+    <div>
+      <Nav />
+      <section>
+        <Logo />
+        <h1></h1>
+        <p></p>
+        <NavLinks>
+          <span></span>
+        </NavLinks>
+      </section>
+    </div>
+  );
+}
+
+export default HomePage;
