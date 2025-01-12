f04b75f8c803052068d6e595d7f4eb7f8ef7d061
bhagwanmmehra <bhagwanmehra04@gmail.com>
2025-01-12 23:32:58 +0530

    Login page set up initiated

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,5 +1,41 @@
+import Logo from "../Components/Logo";
+import Nav from "../Components/Nav";
+import Styles from "../styles/loginPage.module.css";
+
 function LoginPage() {
-  return <div></div>;
+  return (
+    <div className={Styles.loginPage}>
+      <Nav />
+      <main className={Styles.loginSection}>
+        <div className={Styles.loginFormImageBox}>
+          <div className={Styles.imgBox}>
+            <img src="" alt="" />
+          </div>
+
+          <div className={Styles.formBox}>
+            <div className="logoBox">
+              <Logo /> <span></span>
+            </div>
+            <form>
+              <div>
+                <label htmlFor=""></label>
+                <input type="text" />
+              </div>
+              <div>
+                <label htmlFor=""></label>
+                <input type="text" />
+              </div>
+              <div>
+                <label htmlFor=""></label>
+                <input type="text" />
+              </div>
+              <button></button>
+            </form>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default LoginPage;
